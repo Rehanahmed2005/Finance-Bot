d107d9034bc50546c6c07595593923662fafb68d
Rehan Ahmed <Rehanahmed4001@gmail.com>
2026-03-08 17:58:24 +0530

    Function to retrieve Balance

--- src/logic.py
+++ src/logic.py
@@ -13,3 +13,16 @@ def save_transactions(transactions):
     with open("data.json", "w") as f:
         data = [entry.to_dict() for entry in transactions]
         json.dump(data, f, indent=2)
+
+def get_balance(transactionList):
+    income = 0
+    expense = 0
+
+    for transaction in transactionList:
+        if(transaction.trans_type == "income"):
+            income += transaction.amount
+        elif(transaction.trans_type == "expense"):
+            expense += transaction.amount
+
+    Balance = income - expense
+    return Balance
