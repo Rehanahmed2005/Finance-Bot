f775941f70d1b7d13c84499df6312a6f97a9db41
Rehan Ahmed <rehansalmani2005@gmail.com>
2026-04-05 13:42:56 +0530

    inline Button on introduction

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,36 @@
+import logging
 import os
-import asyncio
-import telegram
 
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def main():
-    bot = telegram.Bot(TOKEN)
-    async with bot:
-        await bot.send_message(text='Hi Rehan!', chat_id=6584016896)
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [[InlineKeyboardButton(text="💸 Expense", callback_data="expense"),
+                InlineKeyboardButton(text="💰 Income", callback_data="income")]]
+    
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await context.bot.send_message(
+    chat_id=update.effective_chat.id, 
+    text="V.A.U.L.T. initialized. Your financial system is now active, Mr. Ahmed.",
+    reply_markup = reply_markup
+)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
